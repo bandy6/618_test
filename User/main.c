671d649b05b8e75ade87bb098ad6714a959ff85c
bandy <421977959@qq.com>
2024-12-17 04:01:27 +0800

    【新增】基于stm32标准库3.5实现keil工程

---
+++ User/main.c
@@ -0,0 +1,53 @@
+/**
+  ******************************************************************************
+  * @file    GPIO/IOToggle/main.c 
+  * @author  MCD Application Team
+  * @version V3.5.0
+  * @date    08-April-2011
+  * @brief   Main program body.
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+#include "bsp_led.h"
+
+#define SOFT_DELAY Delay(0x0FFFFF);
+
+void Delay(__IO u32 nCount); 
+
+/**
+  * @brief  主函数
+  * @param  无  
+  * @retval 无
+  */
+int main(void)
+{	
+	/* LED 端口初始化 */
+	LED_GPIO_Config();	 
+
+	while (1)
+	{
+		LED1_ON;			  // 亮
+		SOFT_DELAY;
+		LED1_OFF;		   // 灭
+		SOFT_DELAY;
+	}
+}
+
+void Delay(__IO uint32_t nCount)	 //简单的延时函数
+{
+	for(; nCount != 0; nCount--);
+}
+/*********************************************END OF FILE**********************/
